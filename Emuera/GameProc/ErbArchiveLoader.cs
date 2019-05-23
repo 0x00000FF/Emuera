0b731090089a1c996e3ecb0c35968f590b5c5e87
P.Knowledge <knowledge@patche.me>
2019-05-23 17:23:48 +0900

    comments

--- Emuera/GameProc/ErbArchiveLoader.cs
+++ Emuera/GameProc/ErbArchiveLoader.cs
@@ -18,8 +18,18 @@ namespace MinorShift.Emuera.GameProc
         readonly EmueraConsole output;
 
         #region public
+
+        /// <summary>
+        /// 
+        /// </summary>
         public Dictionary<string, Int64> warningDic = new Dictionary<string, Int64>();
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="main"></param>
+        /// <param name="exm"></param>
+        /// <param name="proc"></param>
         public ErbArchiveLoader(EmueraConsole main, ExpressionMediator exm, Process proc)
         {
             parentProcess = proc;
@@ -27,15 +37,29 @@ namespace MinorShift.Emuera.GameProc
             output = main;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="erbDir"></param>
+        /// <param name="displayReport"></param>
+        /// <param name="labelDirectory"></param>
+        /// <returns></returns>
         public bool LoadErbFiles(string erbDir, bool displayReport, LabelDictionary labelDirectory)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="labelDictionary"></param>
+        /// <returns></returns>
         public bool loadErbs(List<string> path, LabelDictionary labelDictionary)
         {
             throw new NotImplementedException();
         }
+        
         #endregion
 
         #region private
