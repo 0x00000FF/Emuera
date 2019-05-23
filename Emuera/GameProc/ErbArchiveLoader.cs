301ce4a4a4d0b0b20a14428a4680cf3dbd3559d8
P.Knowledge <knowledge@patche.me>
2019-05-23 21:56:28 +0900

    PPState Refactor

--- Emuera/GameProc/ErbArchiveLoader.cs
+++ Emuera/GameProc/ErbArchiveLoader.cs
@@ -17,6 +17,152 @@ namespace MinorShift.Emuera.GameProc
         readonly ExpressionMediator mediator;
         readonly EmueraConsole output;
 
+        #region nested data structure
+        private sealed class PPState
+        {
+            bool skip = false;
+            bool done = false;
+            Stack<bool> disabledStack = new Stack<bool>();
+            Stack<bool> doneStack = new Stack<bool>();
+            Stack<string> ppMatch = new Stack<string>();
+
+            public bool Disabled = false;
+
+            internal void AddKeyword(string token, string token2, ScriptPosition position)
+            {
+                var token2Enabled = (token2?.Length == 0);
+            
+                if (!token2Enabled)
+                {
+                    ParserMediator.Warn(token + "に余分な引数があります", position, 1);
+                }
+                else 
+                {
+                    switch (token)
+                    {
+                        case "SKIPSTART":
+                            if (skip)
+                            {
+                                ParserMediator.Warn("[SKIPSTART]が重複して使用されています", position, 1);
+                                break;
+                            }
+
+                            ppMatch.Push("SKIPEND");
+                            disabledStack.Push(Disabled);
+                            doneStack.Push(done);
+
+                            done = false;
+                            Disabled = skip = true;
+                            break;
+
+                        case "IF_DEBUG":
+                            ppMatch.Push("ELSEIF");
+                            disabledStack.Push(Disabled);
+                            doneStack.Push(done);
+
+                            Disabled = !Program.DebugMode;
+                            done = !Disabled;
+                            break;
+
+                        case "IF_NDEBUG":
+                            ppMatch.Push("ELSEIF");
+                            disabledStack.Push(Disabled);
+                            doneStack.Push(done);
+
+                            Disabled = Program.DebugMode;
+                            done = !Disabled;
+                            break;
+
+                        case "IF":
+                            ppMatch.Push("ELSEIF");
+                            disabledStack.Push(Disabled);
+                            doneStack.Push(done);
+
+                            Disabled = GlobalStatic.IdentifierDictionary.GetMacro(token2) == null;
+                            done = !Disabled;
+                            break;
+
+                        case "ELSEIF":
+                            if (ppMatch.Count == 0 || ppMatch.Pop() != "ELSEIF")
+                            {
+                                ParserMediator.Warn("不適切な[ELSEIF]です", position, 1);
+                                break;
+                            }
+                            ppMatch.Push("ELSEIF");
+                            
+                            Disabled = done || (GlobalStatic.IdentifierDictionary.GetMacro(token2) == null);
+                            done |= !Disabled;
+                            break;
+
+                        case "ELSE":
+                            if (!string.IsNullOrEmpty(token2))
+                            {
+                                ParserMediator.Warn(token + "に余分な引数があります", position, 1);
+                                break;
+                            }
+                            if (ppMatch.Count == 0 || ppMatch.Pop() != "ELSEIF")
+                            {
+                                ParserMediator.Warn("不適切な[ELSE]です", position, 1);
+                                break;
+                            }
+                            ppMatch.Push("ENDIF");
+
+                            Disabled = done;
+                            done = true;
+                            break;
+
+                        case "SKIPEND":
+                            { 
+                                string match = ppMatch.Count == 0 ? "" : ppMatch.Pop();
+                                if (match != "SKIPEND")
+                                {
+                                    ParserMediator.Warn("[SKIPSTART]と対応しない[SKIPEND]です", position, 1);
+                                    break;
+                                }
+                                skip = false;
+                                Disabled = disabledStack.Pop();
+                                done = doneStack.Pop();
+                            }
+                            break;
+
+                        case "ENDIF":
+                            { 
+                                string match = ppMatch.Count == 0 ? "" : ppMatch.Pop();
+                                if (match != "ENDIF" && match != "ELSEIF")
+                                {
+                                    ParserMediator.Warn("対応する[IF]のない[ENDIF]です", position, 1);
+                                    break;
+                                }
+                                Disabled = disabledStack.Pop();
+                                done = doneStack.Pop();
+                            }
+                            break;
+
+                        default:
+                            ParserMediator.Warn("認識できないプリプロセッサです", position, 1);
+                            break;
+                    }
+                }
+
+                if (skip)
+                    Disabled = true;
+            }
+
+            internal void FileEnd(ScriptPosition position)
+            {
+                if (ppMatch.Count != 0)
+                {
+                    var match = ppMatch.Pop();
+                    if (match == "ELSEIF")
+                        match = "ENDIF";
+
+                    ParserMediator.Warn($"[{match}]がありません", position, 1);
+                }
+            }
+
+        }
+        #endregion
+
         #region public
 
         /// <summary>
